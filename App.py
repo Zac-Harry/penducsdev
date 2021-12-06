929c0b3391fd3e8fe67489afe1992e90dc018661
Hugues <hugues.farthouat@cpe.fr>
2021-12-06 17:42:28 +0100

    gzeg

--- App.py
+++ App.py
@@ -1,22 +1,31 @@
 import tkinter as tk
 
-class App(tk.Tk):
+class App():
     def __init__(self):
-        tk.Tk.__init__(self)
         self.init_widgets()
 
     def initGame(self):
         pass
 
     def init_widgets(self):
-        self.canvas = tk.Canvas(self, bg="white", height=500, width=1000)
-        self.canvas.pack()
-        self.word = tk.Label(self, text="M__")
+        self.tk = tk.Tk()
+        self.tk.geometry("1000x500")
+        IMG_Image = tk.PhotoImage(file = "img/bonhomme1.gif")
+        self.canvas = tk.Canvas(self.tk, bg = "white" , width = 500, height = 500)
+        self.canvas.create_image(50,10, image = IMG_Image ,  anchor = "nw")
+        self.canvas.pack(side = "right")
+        # self.canvas.pack()
+        self.word = tk.Label(self.tk, text="M__")
         self.word.pack()
-        self.feedback = tk.Label(self, text="Proposez une lettre")
+        self.feedback = tk.Label(self.tk, text="Proposez une lettre")
         self.feedback.pack()
-        self.submit = tk.Button(self, text="Proposer", command=self.submit_char)
+        self.submit = tk.Button(self.tk, text="Proposer", command=self.submit_char)
         self.submit.pack()
 
     def submit_char(self):
         pass
+
+if __name__ == "__main__":
+    app = App()
+    app.tk.title("Pendu ;(")
+    app.tk.mainloop()
--- graphic.py
+++ graphic.py
@@ -1,8 +1,19 @@
 import tkinter as tk
 import ILib
-import App
+# import App
 
-if __name__ == "__main__":
-    app = App.App()
-    app.title("Pendu ;(")
-    app.mainloop()
+# if __name__ == "__main__":
+#     app = App.App()
+#     app.title("Pendu ;(")
+#     app.mainloop()
+
+TKI_Principal = tk.Tk ( )
+
+IMG_Image = tk.PhotoImage ( file = "img/bonhomme1.gif" )
+
+IMG_Image = tk.PhotoImage(file = "img/bonhomme1.gif")
+canvas = tk.Canvas(TKI_Principal, bg = "white" , width = 1000, height = 500)
+temp = canvas.create_image(50,10, image = IMG_Image ,  anchor = "nw")
+canvas.pack(side = "right")
+
+TKI_Principal.mainloop ( )
